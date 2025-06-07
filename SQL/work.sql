8648c1fad3c595c85e7cf172748c5e39457a12a0
YouchenLin128 <jessie20050128@gmail.com>
2025-06-08 02:32:16 +0800

    Update work.sql

--- SQL/work.sql
+++ SQL/work.sql
@@ -148,6 +148,9 @@ CREATE TABLE `orders` (
   `id` int NOT NULL,
   `buy_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `finalTotal` int NOT NULL,
+  `RecipientName` varchar(100) DEFAULT NULL,
+  `RecipientPhone` varchar(45) DEFAULT NULL,
+  `RecipientAddress` varchar(225) DEFAULT NULL,
   PRIMARY KEY (`OrderID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
