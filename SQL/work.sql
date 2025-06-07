f575c9cfdccc26de708df9e05177e8e4c11d761c
kelly040697 <khyu931108@gmail.com>
2025-06-08 01:03:18 +0800

    再匯一次

--- SQL/work.sql
+++ SQL/work.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `work` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `work`;
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: work
+-- Host: localhost    Database: work
 -- ------------------------------------------------------
--- Server version	8.4.0
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -67,7 +65,7 @@ CREATE TABLE `cart_items` (
 
 LOCK TABLES `cart_items` WRITE;
 /*!40000 ALTER TABLE `cart_items` DISABLE KEYS */;
-INSERT INTO `cart_items` VALUES (1,'14','金平糖',150,1,150,'products/picture2/金平糖.jpg'),(1,'9','聖誕和菓子禮盒組',550,1,550,'sidebar\\picture1\\聖誕和菓子禮盒組.jpg');
+INSERT INTO `cart_items` VALUES (1,'2','日式布丁',125,1,125,'products/picture2/日式布丁.jpg');
 /*!40000 ALTER TABLE `cart_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,14 +82,14 @@ CREATE TABLE `members` (
   `email` varchar(100) NOT NULL,
   `birthday` date NOT NULL,
   `password` varchar(255) NOT NULL,
-  `phone` VARCHAR(20),           -- 加入電話欄位
-  `city` VARCHAR(50),            -- 城市
-  `district` VARCHAR(50),        -- 區域
-  `road` VARCHAR(100),           -- 路段/詳細地址
+  `phone` varchar(20) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `district` varchar(50) DEFAULT NULL,
+  `road` varchar(100) DEFAULT NULL,
   `role` enum('user','admin') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,11 +97,9 @@ CREATE TABLE `members` (
 --
 
 LOCK TABLES `members` WRITE;
-INSERT INTO `members` (`id`, `realname`, `email`, `birthday`, `password`, `phone`, `city`, `district`, `road`, `role`)
-VALUES
-(1, '林', '12345@gmail.com', '2025-05-12', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',
- NULL, NULL, NULL, NULL, 'admin');
-
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES (1,'林','12345@gmail.com','2025-05-12','5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5',NULL,NULL,NULL,NULL,'admin'),(2,'金珉奎有大肌肌','040697@gmail.com','2025-06-11','a90ef855d6ad7ff0a716e37f45f300fe95613959d426b7e593793cec7a4caeec','0906911587','taipei','大安區','大狗狗','user'),(3,'尹淨漢的牛奶晚安曲','1004@gmail.com','2025-06-11','75992a5ac67ff644d3063976c2effd10bdd93fcc109798e3d5c1acf2e530d01a','0906911587','taipei','中正區','忠孝路','user');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -163,6 +159,36 @@ LOCK TABLES `orders` WRITE;
 INSERT INTO `orders` VALUES (2,1,'2025-06-01 13:33:00',240),(3,1,'2025-06-01 13:36:14',80),(4,1,'2025-06-01 13:37:06',290),(5,1,'2025-06-01 13:39:12',940),(6,1,'2025-06-01 13:57:06',160),(7,1,'2025-06-01 13:59:00',160),(8,1,'2025-06-01 14:17:33',160),(9,1,'2025-06-01 14:21:12',160),(10,1,'2025-06-01 14:22:07',160),(11,1,'2025-06-01 14:44:07',1070);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `member_name` varchar(50) NOT NULL,
+  `product_id` int NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `review_content` text,
+  `review_time` datetime NOT NULL,
+  `rating` int NOT NULL DEFAULT '5',
+  PRIMARY KEY (`review_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+INSERT INTO `reviews` VALUES (1,'崔X哲',11,'草莓大福',200.00,'真的超級Q彈，老婆很喜歡~會回購！','2024-05-18 09:25:18',5),(2,'尹X漢',11,'草莓大福',120.00,'超好吃，酸甜口感很棒！','2024-05-18 09:25:18',5),(3,'李X珉',11,'草莓大福',120.00,'超好吃，酸甜口感很棒！','2024-05-18 09:25:18',5),(4,'金X奎',0,'金平糖',80.00,'色彩繽紛又可愛～好喜歡','2024-05-18 09:25:18',5),(5,'李X勳',0,'金平糖',80.00,'色彩繽紛又可愛～好喜歡','2024-05-18 09:25:18',5),(6,'測試者',2,'日式布丁',125.00,'超級好吃！','2025-06-03 01:12:22',5),(7,'蘇',2,'日式布丁',NULL,'會回購!!','2025-06-05 00:37:22',5),(8,'蘇',15,'栗子饅頭',NULL,'很好吃~','2025-06-05 02:00:14',5);
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -173,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-02 13:39:26
+-- Dump completed on 2025-06-08  0:52:03
--- review.jsp
+++ review.jsp
@@ -58,11 +58,11 @@
     <nav>
         <ul>
             <li><a href="index.jsp">首頁</a></li>
-            <li><a href="about.html">關於我們</a></li>
-            <li><a href="register.html">會員註冊</a></li>
-            <li><a href="enter.html">會員登入</a></li>
+            <li><a href="about.jsp">關於我們</a></li>
+            <li><a href="register.jsp">會員註冊</a></li>
+            <li><a href="enter.jsp">會員登入</a></li>
             <li><a href="account.html">會員中心</a></li>
-            <li><a href="shoppingcart.html">購物車</a></li>
+            <li><a href="shoppingcart.jsp">購物車</a></li>
         </ul>
     </nav>
 </header>
